bc965df8613d07cfbccca29a23ba930208cc5413
Philip Pretorius <philippretorius.79@gmail.com>
2023-01-30 22:42:51 +0200

    Provisional (lazy) functions added to js file

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let number;
+
+function add() {
+    number = a + b;
+}
+
+function minus() {
+    number = a - b;
+}
+
+function multiply() {
+    number = a * b;
+}
+
+function divide() {
+    number = a / b;
+}
