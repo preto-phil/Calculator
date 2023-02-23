6fb6e829b5e01304b0c501800d36470f3cb7ad85
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-23 21:37:07 +0200

    JS variables added

---
+++ tut/tut.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButtons = document.querySelectorAll('[data-equals]');
+const deleteButtons = document.querySelectorAll('[data-delete]');
+const allClearButtons = document.querySelectorAll('[data-all-clear]');
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
+
