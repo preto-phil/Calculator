3847d51a6cf806955d53ca3cac24a96bbc3fbea6
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-28 23:22:47 +0200

    Calculator tut progress - Calculator class added

--- tut/tut.js
+++ tut/tut.js
@@ -1,3 +1,38 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear
+    }
+    
+    clear() {
+        this.currentOperandTextElement = ''
+        this.previousOperandTextElement = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand         
+    }
+}
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButtons = document.querySelectorAll('[data-equals]');
@@ -6,3 +41,12 @@ const allClearButtons = document.querySelectorAll('[data-all-clear]');
 const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]');
 const currentOperandTextElement = document.querySelectorAll('[data-current-operand]');
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+
+    })
+})
